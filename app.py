35783a0be3e6f16740415cc167f573dde6c2b471
anjalikota10 <anjalikota161@gmail.com>
2024-10-21 13:18:33 +0530

    edited .py file

--- app.py
+++ app.py
@@ -3,7 +3,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is DevOps Batch 2024!!!'
+    return '''
+    <html>
+        <head>
+            <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                    font-family: Arial, sans-serif;
+                }
+                h1 {
+                    font-weight: bold;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>This is DevOps Batch 2024!!!</h1>
+        </body>
+    </html>
+    '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
