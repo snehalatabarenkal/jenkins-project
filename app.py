1066a90d9d342e4df009996d941b07bc1b16fc48
satishchippa-bob <satishchippa10@gmail.com>
2025-05-14 23:40:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def portfolio():
             </div>
             
             <div class="footer">
-                <p>&copy; 2024 Anjali Kota</p>
+                <p>&copy; 2024 Satish Chippa</p>
             </div>
         </body>
     </html>
